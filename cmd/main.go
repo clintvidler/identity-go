f0c20004c179aa6bff6b72ac1c403df48e7968c1
Clint Vidler <clintvidler@users.noreply.github.com>
2024-03-14 14:24:26 +1100

    Tidy up server and add redoc

--- cmd/main.go
+++ cmd/main.go
@@ -1,66 +1,9 @@
 package main
 
 import (
-	"context"
-	"log"
-	"net"
-	"net/http"
-
-	"github.com/clintvidler/identity-go/gen/proto/go/proto"
-	"github.com/clintvidler/identity-go/service"
-
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"google.golang.org/grpc"
+	"github.com/clintvidler/identity-go/service/server"
 )
 
 func main() {
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	// start the gRPC server
-	lis, err := net.Listen("tcp", "localhost:5566")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	grpcServer := grpc.NewServer()
-	proto.RegisterIdentityServiceServer(grpcServer, &service.IdentityService{})
-	log.Println("gRPC server ready on localhost:5566...")
-	go grpcServer.Serve(lis)
-
-	// dial the gRPC server above to make a client connection
-	conn, err := grpc.Dial("localhost:5566", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
-	}
-	defer conn.Close()
-
-	// create an HTTP router using the client connection above
-	// and register it with the service client
-	rmux := runtime.NewServeMux()
-	client := proto.NewIdentityServiceClient(conn)
-	err = proto.RegisterIdentityServiceHandlerClient(ctx, rmux, client)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// create a standard HTTP router
-	mux := http.NewServeMux()
-
-	// mount the gRPC HTTP gateway to the root
-	mux.Handle("/", rmux)
-
-	// mount a path to expose the generated OpenAPI specification on disk
-	mux.HandleFunc("/swagger-ui/swagger.json", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./gen/protos/service.swagger.json")
-	})
-
-	// mount the Swagger UI that uses the OpenAPI specification path above
-	mux.Handle("/swagger-ui/", http.StripPrefix("/swagger-ui/", http.FileServer(http.Dir("./swagger-ui"))))
-
-	log.Println("HTTP server ready on localhost:8080...")
-	err = http.ListenAndServe("localhost:8080", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
+	server.NewServer().Serve()
 }
