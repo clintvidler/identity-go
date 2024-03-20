1d706711b61fbb9b73f1d72b42693d101c4748e7
Clint Vidler <clintvidler@users.noreply.github.com>
2024-03-20 23:13:01 +1100

    Use gRPC metadata to send and receive tokens instead of args/returns

--- service/rpc/user.go
+++ service/rpc/user.go
@@ -6,19 +6,22 @@ import (
 	"log"
 
 	"github.com/clintvidler/identity-go/gen/proto/go/proto"
+	"google.golang.org/grpc/metadata"
 )
 
 func (s *IdentityService) CurrentUser(ctx context.Context, req *proto.CurrentUserRequest) (*proto.CurrentUserReponse, error) {
 	// TODO: Move some of this into an IsAuth middleware/interceptor
 
-	// Access token must be provided
-	if req.Access == "" {
-		// Unauthorised
-		err := errors.New("no access token provided")
-		return nil, err
+	headers, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errors.New("no metadata found in the context")
+	}
+	access := headers.Get("access")[0]
+	if len(access) < 1 {
+		return nil, errors.New("no token found in the metadata")
 	}
 
-	claims, err := s.ParseClaims(req.Access)
+	claims, err := s.ParseClaims(access)
 	if err != nil {
 		// Unauthorised
 		log.Printf("Error: %s", err)
