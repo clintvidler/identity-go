07197d6d1f571cc01147b5022efc5aa49370b23e
Clint Vidler <clintvidler@users.noreply.github.com>
2024-03-30 16:15:20 +1100

    Create public and private base components on angular client

---
+++ client/ng-client/src/app/private/private.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-private',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './private.component.html',
+  styleUrl: './private.component.scss',
+})
+export class PrivateComponent {}
