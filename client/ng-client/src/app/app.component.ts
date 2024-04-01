36431086688b589d05a28c73f591520b1da7bd73
Clint Vidler <clintvidler@users.noreply.github.com>
2024-04-01 15:35:53 +1100

    Implement 'nav-main' component on Angular client

--- client/ng-client/src/app/app.component.ts
+++ client/ng-client/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavMainComponent } from './components/nav/nav-main/nav-main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavMainComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-client';
