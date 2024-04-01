3ec9e75300f29b4478b2258a227ef30c18be24f7
Clint Vidler <clintvidler@users.noreply.github.com>
2024-04-01 23:27:34 +1100

    * Implement registration on Go server * Add 'display_name' field to user

--- service/data/populate.go
+++ service/data/populate.go
@@ -2,34 +2,36 @@ package data
 
 func (s *Store) Populate() (err error) {
 	u := User{
-		Email: "x@x",
+		Email:       "x@x",
+		DisplayName: "Mr.x",
 	}
 
 	u.SetPassword("x")
 
 	query := `
-		INSERT INTO users (email, password)
-		VALUES ($1, $2)
+		INSERT INTO users (email, password, display_name)
+		VALUES ($1, $2, $3)
 		ON CONFLICT DO NOTHING;
 	`
 
-	if _, err = s.db.Exec(query, u.Email, u.Password); err != nil {
+	if _, err = s.db.Exec(query, u.Email, u.Password, u.DisplayName); err != nil {
 		return
 	}
 
 	u2 := User{
-		Email: "y@y",
+		Email:       "y@y",
+		DisplayName: "Mrs.y",
 	}
 
 	u2.SetPassword("y")
 
 	query2 := `
-		INSERT INTO users (email, password)
-		VALUES ($1, $2)
+		INSERT INTO users (email, password, display_name)
+		VALUES ($1, $2, $3)
 		ON CONFLICT DO NOTHING;
 	`
 
-	if _, err = s.db.Exec(query2, u2.Email, u2.Password); err != nil {
+	if _, err = s.db.Exec(query2, u2.Email, u2.Password, u2.DisplayName); err != nil {
 		return
 	}
 
--- service/rpc/register.go
+++ service/rpc/register.go
@@ -2,25 +2,52 @@ package rpc
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/clintvidler/identity-go/gen/proto/go/proto"
+	"github.com/clintvidler/identity-go/service/data"
 )
 
 func (s IdentityService) RegisterStart(ctx context.Context, req *proto.RegisterStartRequest) (*proto.RegisterStartReponse, error) {
-	log.Printf("RegisterStart(%s, %s)", req.Email, req.Password)
+	key, err := s.data.User.UpsertPendingRegistration(req.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Confirmation key:", key)
+
+	// TODO: Send email
 
 	return &proto.RegisterStartReponse{}, nil
 }
 
 func (s IdentityService) RegisterPending(ctx context.Context, req *proto.RegisterPendingRequest) (*proto.RegisterPendingReponse, error) {
-	log.Println("RegisterPending()", req.Key)
+	email, err := s.data.User.ReadPendingRegistration(req.Key)
+	if err != nil {
+		return nil, err
+	}
 
-	return &proto.RegisterPendingReponse{}, nil
+	return &proto.RegisterPendingReponse{Email: email}, nil
 }
 
 func (s IdentityService) RegisterFinish(ctx context.Context, req *proto.RegisterFinishRequest) (*proto.RegisterFinishReponse, error) {
-	log.Printf("RegisterFinish(%s) %s", req.Key, req.DisplayName)
+	email, err := s.data.User.ReadPendingRegistration(req.Key)
+	if err != nil {
+		return nil, err
+	}
 
-	return &proto.RegisterFinishReponse{}, nil
+	var user data.User
+	user.Email = email
+	user.DisplayName = req.DisplayName
+	user.SetPassword(req.Password)
+
+	id, err := s.data.User.Register(user)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: Send email
+
+	return &proto.RegisterFinishReponse{Id: fmt.Sprint(id)}, nil
 }
